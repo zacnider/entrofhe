cd50e6c524007b67bfe273dc04d92837d903df0a
zacnider <nihataltuntas@gmail.com>
2025-12-10 16:42:29 +0300

    Update handlers to save events to database using Envio context API

--- indexer/entropy-oracle/src/EntropyOracleHandlers.ts
+++ indexer/entropy-oracle/src/EntropyOracleHandlers.ts
@@ -7,47 +7,89 @@ const EntropyOracle = generated.EntropyOracle || generated;
 // Handler for EntropyRequested event
 console.log('[EntropyOracleHandlers] Registering EntropyRequested handler...');
 EntropyOracle.EntropyRequested.handler(async ({ event, context }) => {
-  console.log("EntropyRequested", {
-    requestId: event.params.requestId?.toString?.() ?? event.params.requestId,
-    hashedConsumer: event.params.hashedConsumer,
-    hashedTag: event.params.hashedTag,
-    feePaid: event.params.feePaid?.toString?.() ?? event.params.feePaid,
-    txHash: event.transaction?.hash,
-    blockNumber: event.block?.number,
-  });
+  try {
+    const requestId = BigInt(event.params.requestId?.toString() || event.params.requestId);
+    const id = `${event.transaction.hash}-${event.logIndex || 0}`;
+    
+    context.EntropyRequested.set({
+      id: id,
+      requestId: requestId,
+      hashedConsumer: event.params.hashedConsumer,
+      hashedTag: event.params.hashedTag,
+      feePaid: BigInt(event.params.feePaid?.toString() || event.params.feePaid),
+      txHash: event.transaction.hash,
+      blockNumber: BigInt(event.block.number),
+    });
+    
+    console.log(`[EntropyRequested] Saved: requestId=${requestId}, txHash=${event.transaction.hash}`);
+  } catch (error) {
+    console.error('[EntropyRequested] ERROR:', error);
+    throw error;
+  }
 });
 
 // Handler for EntropyFulfilled event
 console.log('[EntropyOracleHandlers] Registering EntropyFulfilled handler...');
 EntropyOracle.EntropyFulfilled?.handler(async ({ event, context }) => {
-  console.log("EntropyFulfilled", {
-    requestId: event.params.requestId?.toString?.() ?? event.params.requestId,
-    hashedConsumer: event.params.hashedConsumer,
-    hashedTag: event.params.hashedTag,
-    txHash: event.transaction?.hash,
-    blockNumber: event.block?.number,
-  });
+  try {
+    const requestId = BigInt(event.params.requestId?.toString() || event.params.requestId);
+    const id = `${event.transaction.hash}-${event.logIndex || 0}`;
+    
+    context.EntropyFulfilled.set({
+      id: id,
+      requestId: requestId,
+      hashedConsumer: event.params.hashedConsumer,
+      hashedTag: event.params.hashedTag,
+      txHash: event.transaction.hash,
+      blockNumber: BigInt(event.block.number),
+    });
+    
+    console.log(`[EntropyFulfilled] Saved: requestId=${requestId}, txHash=${event.transaction.hash}`);
+  } catch (error) {
+    console.error('[EntropyFulfilled] ERROR:', error);
+    throw error;
+  }
 });
 
 // Handler for FeeRecipientUpdated event
 console.log('[EntropyOracleHandlers] Registering FeeRecipientUpdated handler...');
 EntropyOracle.FeeRecipientUpdated?.handler(async ({ event, context }) => {
-  console.log("FeeRecipientUpdated", {
-    oldRecipient: event.params.oldRecipient,
-    newRecipient: event.params.newRecipient,
-    txHash: event.transaction?.hash,
-    blockNumber: event.block?.number,
-  });
+  try {
+    const id = `${event.transaction.hash}-${event.logIndex || 0}`;
+    
+    context.FeeRecipientUpdated.set({
+      id: id,
+      oldRecipient: event.params.oldRecipient,
+      newRecipient: event.params.newRecipient,
+      txHash: event.transaction.hash,
+      blockNumber: BigInt(event.block.number),
+    });
+    
+    console.log(`[FeeRecipientUpdated] Saved: txHash=${event.transaction.hash}`);
+  } catch (error) {
+    console.error('[FeeRecipientUpdated] ERROR:', error);
+    throw error;
+  }
 });
 
 // Handler for ChaosEngineUpdated event
 console.log('[EntropyOracleHandlers] Registering ChaosEngineUpdated handler...');
 EntropyOracle.ChaosEngineUpdated?.handler(async ({ event, context }) => {
-  console.log("ChaosEngineUpdated", {
-    oldEngine: event.params.oldEngine,
-    newEngine: event.params.newEngine,
-    txHash: event.transaction?.hash,
-    blockNumber: event.block?.number,
-  });
+  try {
+    const id = `${event.transaction.hash}-${event.logIndex || 0}`;
+    
+    context.ChaosEngineUpdated.set({
+      id: id,
+      oldEngine: event.params.oldEngine,
+      newEngine: event.params.newEngine,
+      txHash: event.transaction.hash,
+      blockNumber: BigInt(event.block.number),
+    });
+    
+    console.log(`[ChaosEngineUpdated] Saved: txHash=${event.transaction.hash}`);
+  } catch (error) {
+    console.error('[ChaosEngineUpdated] ERROR:', error);
+    throw error;
+  }
 });
 
